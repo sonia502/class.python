c7f72a657aab1be746865cd0cf7c3e9251ec85cf
sonia saleem <jeemano650@gmail.com>
2026-02-24 14:06:06 +0500

    its a second comit

---
+++ data.py
@@ -0,0 +1,58 @@
+print ("hello sonia")
+
+price = 450
+salary = 4500.45
+name = "ali"
+items = ["apple", "book","laptop","chair"]
+static_items = ("u009","123456",36)
+user_info = {'name':"hamza",'age':22,"address":"abc multan pak"}
+base_values = {12,34,45,56,12}
+
+# function
+# 23,45
+# 1234,453
+# 45,2342424
+
+def sum(val1,val2):
+    return val1 + val2
+
+value1 = 23
+value2 = 45
+sum1 = value1 + value2
+print("sum1 ans:", sum1)
+# adding sum1 values by using function sum
+print("adding sum1 values by using function sum",sum(23,45))
+
+value101 = 1234
+value201 = 453
+sum2 = value101 + value201
+print("sum2 ans:", sum2)
+# adding sum2 values by using function sum
+print("adding sum2 values by using function sum",sum(value101,value201))
+
+value1011 = 45
+value2011 =  2342424
+sum3 = value1011 + value2011
+print("sum3 ans:", sum3)
+# adding sum3 values by using function sum
+print("adding sum3 values by using function sum",sum(45,2342424))
+
+
+res = sum(2000034,345)
+print(res)
+
+
+
+print("we arelearning asdfasfasf", value1011)
+
+
+price = 450
+salary = 4500.45
+name = "ali"
+items = ["apple", "book","laptop","chair"]
+static_items = ("u009","123456",36)
+user_info = {'name':"hamza",'age':22,"address":"abc multan pak"}
+base_values = {12,34,45,56,12}
+
+print(type(name))
+print(type(user_info))
--- datatype.py
+++ datatype.py
@@ -1,1 +1,6 @@
 print ("hello world")
+num_1 = 10
+num_2 = 20
+num_3 =30.3
+result = (num_1,num_2,num_3)
+print (result)
