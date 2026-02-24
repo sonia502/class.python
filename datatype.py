4989ebcdc1f98e8bc142f085728a58c6947712cd
sonia saleem <jeemano650@gmail.com>
2026-02-24 14:55:37 +0500

    this is the input funtion

--- datatype.py
+++
@@ -1,6 +0,0 @@
-print ("hello world")
-num_1 = 10
-num_2 = 20
-num_3 =30.3
-result = (num_1,num_2,num_3)
-print (result)
---
+++ input-output and operation.py
@@ -0,0 +1,9 @@
+# ad two values.
+num1 = 23
+num2 = 4757
+add =num1 + num2
+res = add/2
+print (add)
+
+
+input()
