59d56824990118fdf92a4c48c6f451fcea97042d
sonia saleem <jeemano650@gmail.com>
2026-02-28 15:33:47 +0500

    its prctice else

---
+++ if-else.py
@@ -0,0 +1,22 @@
+# age = float(input("enter your age"))
+
+# if age >= 18:
+#     print("welcome you're able to proceed your ID card")
+
+
+# marks = float(input("enter your marks"))
+
+# if marks >=60:
+#     print("Congratulations you're passed")
+# else:
+#     print("Sorry you're failed, try again")
+
+
+# check whether the number is positive or negative
+
+number = int(input("please enter a number of your choice>>"))
+
+if number >0:
+    print("The Number",number, " is Positive")
+else:
+    print("The number", number ,"is Negative")
